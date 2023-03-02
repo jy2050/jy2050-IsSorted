64fba54df22b48dbe4e83b9315b34b8a5e7c513c
Jay Y <alforoanjay@gmail.com>
2023-03-02 03:33:43 +0000

    Completed IsSorted Lab

--- src/main/java/IsSorted.java
+++ src/main/java/IsSorted.java
@@ -7,6 +7,20 @@ public class IsSorted {
      * @return true if arr is sorted (all the numbers are in order). false if arr is not sorted.
      */
     public boolean checkSort(int[] arr){
-        return false;
+        int count = 0;
+        int start = 0;
+        for(int i=0; i<arr.length; i++){
+            if(arr[i] > start){
+                start = arr[i];
+            }else if(arr[i] < start){
+                count++;
+            }
+        }
+        if(count != 0){
+            return false;
+        }else{
+            return true;
+        }
+       
     }
 }
